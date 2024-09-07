ff9563cd98dc7d3eabf6765f5fb0496f44b9b352
rachelYeffe <161024924+rachelYeffe@users.noreply.github.com>
2024-09-08 02:20:03 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000,  // שימוש במשתנה הסביבה PORT
+    host: '0.0.0.0',                 // הקשבה לכל הכתובות (ולא רק ל-localhost)
+  },
 })
